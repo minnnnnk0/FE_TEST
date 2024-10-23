21fac2061d702403563ea6f7c884d203b8511476
minnnnnk0 <kjjpyckjs@naver.com>
2024-10-23 21:40:51 +0900

    Jest: HTTP Request 모킹

--- src/__jest__/Login.spec.tsx
+++ src/__jest__/Login.spec.tsx
@@ -1,22 +1,32 @@
 import '@testing-library/jest-dom';
+
+import * as nock from "nock"
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { render, renderHook, waitFor, screen, fireEvent } from '@testing-library/react';
+import {
+  render,
+  renderHook,
+  waitFor,
+  screen,
+  fireEvent,
+} from '@testing-library/react';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import useLogin from '../hooks/useLogin';
 
 const queryClient = new QueryClient({
   defaultOptions: {},
-  logger: {
-    log: console.log,
-    warn: console.warn,
-    error: process.env.NODE_ENV === "test" ? () => {} : console.error
-  }
 });
 
 describe('로그인 테스트', () => {
-  test('로그인에 실패하면 에러 메세지가 나타난다.', async () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
 
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('로그인에 실패하면 에러 메세지가 나타난다.', async () => {
     // given - 로그인 화면이 랜더링
     const routes = [
       {
@@ -37,18 +47,24 @@ describe('로그인 테스트', () => {
     );
 
     // when - 사용자가 로그인에 실패
+    nock("").post("/user/login/", {
+        username: "wrong@email.com",
+        password: "wrongPassword",
+      })
+      .reply(400, { msg: "NO_SUCH_USER" });
+
     const wrapper = ({ children }) => (
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     );
 
-    const emailInput = screen.getByLabelText("이메일")
-    const passwordInput = screen.getByLabelText("비밀번호")
+    const emailInput = screen.getByLabelText('이메일');
+    const passwordInput = screen.getByLabelText('비밀번호');
 
-    fireEvent.change(emailInput, {target: {value: "wrong@email.com"}})
-    fireEvent.change(passwordInput, {target: {value: "wrongPassword"}})
+    fireEvent.change(emailInput, { target: { value: 'wrong@email.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'wrongPassword' } });
 
-    const loginButton = screen.getByRole("button", {name: "로그인"})
-    fireEvent.click(loginButton)
+    const loginButton = screen.getByRole('button', { name: '로그인' });
+    fireEvent.click(loginButton);
 
     const { result } = renderHook(() => useLogin(), { wrapper });
 
@@ -57,6 +73,5 @@ describe('로그인 테스트', () => {
 
     const errorMessage = await screen.findByTestId('error-message');
     expect(errorMessage).toBeInTheDocument();
-
   });
 });
