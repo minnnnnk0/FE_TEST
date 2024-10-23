1eff0d00e5bbefed8af807435c06d393e934f7d9
minnnnnk0 <kjjpyckjs@naver.com>
2024-10-23 19:13:32 +0900

    Jest: 성공 테스트케이스 작성

--- src/__jest__/Signup.spec.tsx
+++ src/__jest__/Signup.spec.tsx
@@ -5,12 +5,13 @@ import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient({
-    defaultOptions: {},
+  defaultOptions: {},
 });
 
 describe('회원가입 테스트', () => {
-  test('비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러 메세지가 표시된다.', async () => {
-    
+  // 중복되는 부분은 하나로 처리
+  // 각 test가 돌아가기 전마다 실행
+  beforeEach(() => {
     // given - 회원가입 페이지가 랜더링
     const routes = [
       {
@@ -29,19 +30,43 @@ describe('회원가입 테스트', () => {
         <RouterProvider router={router} />
       </QueryClientProvider>
     );
+  });
 
+  test('비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러 메세지가 표시된다.', async () => {
     // when - 비밀번호와 비밀번호 확인 값이 일치하지 않음
-    const passwordInput = screen.getByLabelText("비밀번호")
-    const confirmPasswordInput = screen.getByLabelText("비밀번호 확인")
+    const passwordInput = screen.getByLabelText('비밀번호');
+    const confirmPasswordInput = screen.getByLabelText('비밀번호 확인');
 
-    fireEvent.change(passwordInput, {target: {value: "password"}})
+    fireEvent.change(passwordInput, { target: { value: 'password' } });
     fireEvent.change(confirmPasswordInput, {
-        target: {value: "WrongPassword"}
-    })
+      target: { value: 'WrongPassword' },
+    });
 
     // then - 에러 메세지가 표시
-    const errorMessage = await screen.findByTestId("error-message")
-    expect(errorMessage).toBeInTheDocument()
+    const errorMessage = await screen.findByTestId('error-message');
+    expect(errorMessage).toBeInTheDocument();
+  });
 
+  test('이메일을 입력하고, 비빌번호와 비밀번호 확인 값이 일치하면 회원가입 버튼이 활성화된다.', () => {
+    // 회원가입 버튼이 비활성화 되어있는 것을 확인하고 시작하면 좋음
+    const signupButton = screen.getByRole('button', { name: '회원가입' });
+    expect(signupButton).toBeDisabled();
+
+    // when - 이메일 입력, 비밀번호, 비밀번호 확인 일치
+    const emailInput = screen.getByLabelText('이메일');
+    const passwordInput = screen.getByLabelText('비밀번호');
+    const confirmPasswordInput = screen.getByLabelText('비밀번호 확인');
+
+    fireEvent.change(emailInput, {
+      target: { value: 'button-activate@email.com' },
+    });
+    fireEvent.change(passwordInput, { target: { value: 'password' } });
+    fireEvent.change(confirmPasswordInput, {
+      target: { value: 'password' },
+    });
+
+    // then - 회원가입 버튼 활성화
+    expect(signupButton).toBeEnabled();
+    
   });
 });
