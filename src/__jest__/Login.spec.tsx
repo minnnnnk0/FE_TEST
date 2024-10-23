f4e3eb8ab4538f7c1eb8c7cd5d7a82860621860c
minnnnnk0 <kjjpyckjs@naver.com>
2024-10-23 20:57:24 +0900

    Jest: logger 옵션 추가

--- src/__jest__/Login.spec.tsx
+++ src/__jest__/Login.spec.tsx
@@ -1,12 +1,17 @@
 import '@testing-library/jest-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { render, renderHook, waitFor, screen } from '@testing-library/react';
+import { render, renderHook, waitFor, screen, fireEvent } from '@testing-library/react';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import useLogin from '../hooks/useLogin';
 
 const queryClient = new QueryClient({
   defaultOptions: {},
+  logger: {
+    log: console.log,
+    warn: console.warn,
+    error: process.env.NODE_ENV === "test" ? () => {} : console.error
+  }
 });
 
 describe('로그인 테스트', () => {
@@ -36,11 +41,20 @@ describe('로그인 테스트', () => {
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     );
 
+    const emailInput = screen.getByLabelText("이메일")
+    const passwordInput = screen.getByLabelText("비밀번호")
+
+    fireEvent.change(emailInput, {target: {value: "wrong@email.com"}})
+    fireEvent.change(passwordInput, {target: {value: "wrongPassword"}})
+
+    const loginButton = screen.getByRole("button", {name: "로그인"})
+    fireEvent.click(loginButton)
+
     const { result } = renderHook(() => useLogin(), { wrapper });
 
     // then - 에러 메세지가 나타남
     await waitFor(() => result.current.isError);
-    
+
     const errorMessage = await screen.findByTestId('error-message');
     expect(errorMessage).toBeInTheDocument();
 
