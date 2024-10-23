d8602d2e5ace002783ebf20f8ff673d123a59e45
minnnnnk0 <kjjpyckjs@naver.com>
2024-10-23 18:25:11 +0900

    Jest: 실패 테스트케이스 작성

--- src/__jest__/Signup.spec.tsx
+++ src/__jest__/Signup.spec.tsx
@@ -1,5 +1,37 @@
-describe("환경설정 테스트", () => {
-    test("환경설정 테스트", () => {
-        
-    })
-})
+import 'testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import SignupPage from 'pages/SignupPage';
+import { createMemoryRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+    defaultOptions: {},
+});
+
+describe('회원가입 테스트', () => {
+  test('비밀번호와 비밀번호 확인 값이 일치하지 않으면 에러 메세지가 표시된다.', () => {
+    // given - 회원가입 페이지가 랜더링
+
+    const routes = [
+      {
+        path: '/signup',
+        element: <SignupPage />,
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ['/signup'],
+      initialIndex: 0,
+    });
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    );
+
+    // when - 비밀번호와 비밀번호 확인 값이 일치하지 않음
+
+    // then - 에러 메세지가 표시
+  });
+});
