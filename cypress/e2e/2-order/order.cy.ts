bb6f7c640bd80d6949d53efbf5135191dfe62499
minnnnnk0 <kjjpyckjs@naver.com>
2024-10-31 01:28:20 +0900

    Cypress: 공용 컴포넌트 테스트

--- cypress/e2e/2-order/order.cy.ts
+++ cypress/e2e/2-order/order.cy.ts
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
+describe('오더를 테스트 한다.', () => {
+  it('사용자는 딜리버리/픽업 중 원하는 유형을 선택할 수 있다.', () => {
+    cy.visit("/")
+
+    cy.get("[data-cy=deliveryBtn]").should("be.visible").as("deliveryBtn")
+    cy.get("[data-cy=pickupBtn]").should("be.visible").as("pickupBtn")
+
+    cy.get("@deliveryBtn").click()
+    cy.url().should("include", "/food-type")
+
   })
 })
